5fc18f1b5cc91ea2cecf1f569df9d4aa94d51cef
ikakarotto <43361811+ikakarotto@users.noreply.github.com>
2020-05-22 15:34:31 +0800

    支持翻页获取疫情通报情况

--- get_newpneumonia_guangzhou.py
+++ get_newpneumonia_guangzhou.py
@@ -2,12 +2,30 @@
 # -*- coding: utf-8 -*-
 # Description: 获取广州卫健委疫情通报情况
 
+import time
+import random
 import re
 import urllib.request
 from bs4 import BeautifulSoup
 
-# 获取【疫情通报】第一页的url以及标题
+# 获取分页的url地址
+def get_next_page(urls):
+    url = urls[-1]
+    request=urllib.request.Request(url,headers=headers)
+    response=urllib.request.urlopen(request).read()
+    response = response.decode("UTF-8")
+    try:
+        next_url = re.findall(r'<a class="next" href="(.*)">', response)[0]
+        if next_url:
+            urls.append(next_url)
+            get_next_page(urls)
+    except:
+        pass
+    return urls
+
+# 获取当前页的新闻url以及标题
 def get_days_url(url):
+    time.sleep(random.choice(range(1,11)))
     request=urllib.request.Request(url,headers=headers)
     response=urllib.request.urlopen(request).read()
     response = response.decode("UTF-8")
@@ -16,23 +34,27 @@ def get_days_url(url):
 
 # 获取指定url中的疫情通报情况
 def get_day_news(url):
+    time.sleep(random.choice(range(1,11)))
     request=urllib.request.Request(url,headers=headers)
     response=urllib.request.urlopen(request).read()
     response = response.decode("UTF-8")
-    m = re.search(r'(<p indenttext=".*</p>|<p style.*</p>)', response, flags=re.I)
+    m = re.search(r'(<p indenttext=".*</p>|<p style.*</p>)|<p><span style.*</span></p>', response, flags=re.I)
     news = m.group()
     data = BeautifulSoup(news, "lxml").text
     return data
 
 
 if __name__ == '__main__':
-    url = 'http://wjw.gz.gov.cn/ztzl/xxfyyqfk/yqtb/index.html'
+    urls = ['http://wjw.gz.gov.cn/ztzl/xxfyyqfk/yqtb/index.html']
     headers ={
        'User-Agent':'Mozilla/5.0 (X11; Fedora; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
     }
-    for dayinfo in get_days_url(url):
-        dayurl = dayinfo[0]
-        daytitle = dayinfo[1]
-        print('============================' + daytitle + '============================')
-        daydata = get_day_news(dayurl)
-        for i in daydata.split(): print(i)
+    for page_url in get_next_page(urls):
+        for dayinfo in get_days_url(page_url):
+            dayurl = dayinfo[0]
+            daytitle = dayinfo[1]
+            print('============================' + daytitle + '============================')
+            daydata = get_day_news(dayurl)
+            for i in daydata.split('\u3000\u3000'):
+                print(i)
+            print()
